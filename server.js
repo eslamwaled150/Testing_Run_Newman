6e710c61443361e6bf69eb17369809e26f0257cd
islam waled <eslamwaled150@gmail.com>
2025-03-25 16:15:19 +0200

    Added db.json file

---
+++ server.js
@@ -0,0 +1,17 @@
+const jsonServer = require("json-server");
+const express = require("express");
+const cors = require("cors");
+
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+
+server.use(cors());
+server.use(express.json());
+server.use(middlewares);
+server.use(router);
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`JSON Server is running on port ${PORT}`);
+});
